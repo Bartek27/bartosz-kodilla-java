a7185c7c1a21f149e13ad3d089765ab7f6bbbf5f
Bartek27 <bartoszszarek1gmail.com>
2022-03-18 20:12:02 +0100

    First Commit

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,40 @@
+package com.kodilla.stream.world;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public final class Continent {
+
+    private final String continentName;
+    private final Set<Country> listOfCountries = new HashSet<>();
+
+    public Continent(String continentName) {
+        this.continentName = continentName;
+    }
+
+    public void addCountry(Country country){
+        listOfCountries.add(country);
+    }
+
+    public Set<Country> getListOfCountries() {
+        return listOfCountries;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Continent continent = (Continent) o;
+        return Objects.equals(continentName, continent.continentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return continentName.hashCode();
+    }
+
+    @Override
+    public String toString(){
+        return continentName + " zawiera: " + listOfCountries;
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.world;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class World {
+
+    private final Set<Continent> listOfContinents = new HashSet<>();
+
+    public Set<Continent> getListOfContinents() {
+        return listOfContinents;
+    }
+
+    public final BigDecimal getPeopleQuantity(){
+        return listOfContinents.stream()
+                .flatMap(continent -> continent.getListOfCountries().stream())
+                .map(Country::getPeopleQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) ->  sum = sum.add(current));
+    }
+
+    @Override
+    public String toString(){
+        return "W moim zmyślonym świecie znajduje się " + getPeopleQuantity() + " ludzi";
+    }
+}
