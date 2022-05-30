9d9463126ced7f4ca8280a5af5b0d4ebd52e0722
Bartek27 <bartoszszarek1gmail.com>
2022-05-30 23:11:34 +0200

    First Commit

---
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/challenges/flight/FlightFinderRunner.java
@@ -0,0 +1,25 @@
+package com.kodilla.good.paterns.challenges.flight;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static com.kodilla.good.paterns.challenges.flight.MapOfAvailableFlights.getMapOfAvailableFlights;
+
+public class FlightFinderRunner {
+
+    public static void main(String args[]) {
+
+        String departureAirport = "Warsaw";
+        Optional<String> intermediateAirport = Optional.ofNullable("Cracow");
+        String arrivalAirport = "Gdansk";
+
+        Flight flight = new Flight(departureAirport, intermediateAirport, arrivalAirport);
+        FlightFinder flightFinder = new FlightFinder();
+
+        Map<Integer, Flight> mapOfAvailableFlights = getMapOfAvailableFlights();
+
+        flightFinder.findFlightWithIntermediateAirport(flight, mapOfAvailableFlights);
+        flightFinder.findFlightFromAirport(departureAirport, mapOfAvailableFlights);
+        flightFinder.findFlightToAirport(arrivalAirport, mapOfAvailableFlights);
+    }
+}
---
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/challenges/flight/MapOfAvailableFlights.java
@@ -0,0 +1,20 @@
+package com.kodilla.good.paterns.challenges.flight;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public final class MapOfAvailableFlights {
+
+    public static Map<Integer, Flight> getMapOfAvailableFlights() {
+
+        Map<Integer, Flight> mapOfAvailableFlights = new HashMap<>();
+        mapOfAvailableFlights.put(1, new Flight("Warsaw", Optional.ofNullable("Gdansk"), "Cracow"));
+        mapOfAvailableFlights.put(2, new Flight("Gdansk", Optional.empty(),"Warsaw"));
+        mapOfAvailableFlights.put(3, new Flight("Warsaw", Optional.empty(), "Gdansk"));
+        mapOfAvailableFlights.put(4, new Flight("Gdansk", Optional.ofNullable("Warsaw"), "Rzeszow"));
+        mapOfAvailableFlights.put(3, new Flight("Warsaw", Optional.ofNullable("Cracow"), "Gdansk"));
+
+        return mapOfAvailableFlights;
+    }
+}
